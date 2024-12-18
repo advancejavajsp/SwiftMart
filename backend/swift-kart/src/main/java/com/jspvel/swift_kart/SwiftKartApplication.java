ae1a1ef7e26a64cd62a79f031d74f1aad5d4c656
anvith <anwithgowda2@gmail.com>
2024-12-18 20:07:35 +0530

    back end structure 1

---
+++ backend/swift-kart/src/main/java/com/jspvel/swift_kart/SwiftKartApplication.java
@@ -0,0 +1,13 @@
+package com.jspvel.swift_kart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftKartApplication.class, args);
+	}
+
+}
