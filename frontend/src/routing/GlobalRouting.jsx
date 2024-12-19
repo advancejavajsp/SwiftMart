65560d8e15f3f5ea43fba6d09657c37e81199616
JanhviTripathi <tripathijanhvi2003@gmail.com>
2024-12-19 17:22:32 +0530

    remove the previous code

--- frontend/src/routing/GlobalRouting.jsx
+++ frontend/src/routing/GlobalRouting.jsx
@@ -2,10 +2,6 @@ import {createBrowserRouter} from "react-router-dom";
 import Layout from "../Component/Layout";
 import HomePage from "../Component/HomePage/HomePage";
 import ErrorPage from "../Component/PageNotFound/ErrorPage";
-import Orders from '../Orders/Order';
-import OrderDetails from "../OrderDetails/OrderDetails";
-import Payments from "../Payments/Payments";
-import PaymentDetails from "../PaymentDetails/PaymentDetails";
 export const globalRoute=createBrowserRouter([
     {
       path:"/",
@@ -15,23 +11,7 @@ export const globalRoute=createBrowserRouter([
             path:"/homePage",
             element:<HomePage/>,
             children:[
-             {
-              path: "/orderPage",
-              element : <Orders/>
-             },
-             {
-              path:"/orderdetailsPage",
-              element : <OrderDetails/>
-             },
-             {
-              path:"/payments",
-              element : <Payments/>  
-
-             },
-             {
-              path:"/paymentdetailsPage",
-              element:<PaymentDetails/>
-             }
+             
             ]
         }
       ]
