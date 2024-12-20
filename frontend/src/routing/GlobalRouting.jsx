1b132d05f8b75448042de37b7a0f92a86b5387c1
Shivam jha <shivamjha005@gmail.com>
2024-12-20 11:25:26 +0530

    stage changes

--- frontend/src/routing/GlobalRouting.jsx
+++ frontend/src/routing/GlobalRouting.jsx
@@ -2,33 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Component/Layout";
 import HomePage from "../Component/HomePage/HomePage";
 import ErrorPage from "../Component/PageNotFound/ErrorPage";
-<<<<<<< HEAD
-import Navbar from "../Component/Navbar/Navbar.jsx";
-
-export const globalRoute=createBrowserRouter([
-    {
-      path:"/",
-      element:<Layout/>,
-      children:[
-        {
-            path:"/homePage",
-            element:<HomePage/>,
-            children:[
-              {
-                path:"/navbar",
-                element:<Navbar/>
-            },
-            
-             
-            ]
-        }
-      ]
-    },   {
-        path:"*",
-        element:<ErrorPage/>
-    },
-])
-=======
 import Status from "../Component/Delivery/Status";
 import Payment from "../Component/payment/Payment";
 import PaymentSucessful from "../Component/paymentSuccessful/PaymentSucessful";
@@ -62,4 +35,3 @@ export const globalRoute = createBrowserRouter([
     element: <ErrorPage />,
   },
 ]);
->>>>>>> 9fec58da6204429cf3ca8cd18c2f2edbb07b17be
