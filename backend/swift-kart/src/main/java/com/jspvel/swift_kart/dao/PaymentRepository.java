a05214eee78fe938fb0e533a599576499a28f042
Gusainvandana <gusainvandana6@gmail.com>
2024-12-21 16:38:55 +0530

    repositories for all the entites

---
+++ backend/swift-kart/src/main/java/com/jspvel/swift_kart/dao/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.jspvel.swift_kart.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+	
+
+}
